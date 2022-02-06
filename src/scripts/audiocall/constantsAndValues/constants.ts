88bcc1bb12afc4927485e77f3f015ca8c53e0bf4
anna-left <anna_left@mail.ru>
2022-02-06 10:33:40 +0300

    feat: create an array of questions

---
+++ src/scripts/audiocall/constantsAndValues/constants.ts
@@ -0,0 +1,2 @@
+export const NUMBER_OF_WORDS_IN_ROUND = 10;
+export const NUMBER_OF_ANCWERS = 5;
---
+++ src/scripts/audiocall/constantsAndValues/globalValues.ts
@@ -0,0 +1,6 @@
+const GLOBAL_VALUES = {
+  currentRound: 0,
+  currentQuestion: 0,
+};
+
+export { GLOBAL_VALUES };
