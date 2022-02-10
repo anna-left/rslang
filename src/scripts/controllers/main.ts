daac07aa6cbf362be1c686086ab406e85e2e432b
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-02-10 12:09:55 +0300

    refactor: remove unnecessary files 2022-02-10 (12:09:55)

--- src/scripts/controllers/main.ts
+++ src/scripts/controllers/main.ts
@@ -1,8 +1,4 @@
 import '../main/auth/authContorller';
 
 import './games/Audiocall';
-import './games/Original';
-import './games/Puzzle';
-import './games/Savannah';
-import './games/SpeakIt';
 import './games/Sprint';
