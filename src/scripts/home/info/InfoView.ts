a16afdd54ae07480a3e23ca3650da4463e1b148b
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-02-14 18:50:46 +0300

    feat: render correct info page info 2022-02-14 (18:50:45)

--- src/scripts/home/info/InfoView.ts
+++ src/scripts/home/info/InfoView.ts
@@ -2,6 +2,7 @@ import { createElement } from '../../util/Util';
 import { IInfo } from './IInfo';
 
 import './info.scss';
+import { getHiddenSvgs, getSvgs } from './InfoBuilder';
 
 export class Info {
   constructor(rootEl: HTMLElement) {
@@ -9,13 +10,21 @@ export class Info {
       const data: IInfo = await res.json();
       rootEl.innerHTML = '';
       const section = createElement('section', ['main-box__section_type_info', 'section-info']);
-      const header = createElement('h2', ['section-info__header']);
-      const infoList = createElement('ul', ['section-info__info-list']);
+      const header = createElement('h2', ['section-info__header'], [], 'Интересные факты');
+      const infoList = createElement('ul', ['section-info__info-list', 'info-list']);
 
+      section.append(getHiddenSvgs());
+      const svgs = getSvgs();
       for (let i = 0; i < data.info.length; i++) {
-        infoList.append(
-          createElement('li', ['section-info__info-item'], [['title', data.details[i]]], `${i + 1}. ${data.info[i]}`),
+        const item = createElement('li', ['info-list__item'], []);
+        const text = createElement(
+          'span',
+          ['info-list__item_text'],
+          [['alt', data.details[i].join('\n')]],
+          `${i + 1}. ${data.info[i]}`,
         );
+        item.append(svgs[i], text);
+        infoList.append(item);
       }
 
       section.append(header, infoList);
