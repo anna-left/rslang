02e0672611b44cdac653ad4d9cfe7913818714e3
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-02-14 22:09:57 +0300

    fix: few fixes accordint to review 2022-02-14 (22:09:56)

--- src/scripts/home/review/ReviewView.ts
+++ src/scripts/home/review/ReviewView.ts
@@ -6,8 +6,9 @@ export class Review {
   constructor(rootEl: HTMLElement) {
     rootEl.innerHTML = '';
     const section = createElement('section', ['main-box__section', 'main-box__section_type_review', 'section-review']);
+    const header = createElement('h2', ['section-review__header'], [], 'Обзор');
     const videoBox = createElement('div', ['section-review__video-box']);
-    section.append(videoBox);
+    section.append(header, videoBox);
     rootEl.append(section);
   }
 }
