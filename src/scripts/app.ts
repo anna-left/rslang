80dd1d7fb727a388d647e2280c7ae13fd4583b37
valentine909 <valentine.nazarov@gmail.com>
2022-02-17 12:24:59 +0300

    fix: fix dictionary and sprint initialization

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,31 +1,11 @@
-import Dictionary from './dictionary/Dictionary';
-import Sprint from './sprint/Sprint';
-import { LocalStorage } from './state/StorageSettings';
 import { ViewManager } from './manager/ViewManager';
-import Modal from "./api/Modal";
 
 async function init() {
-  const sprint = new Sprint();
-  const dict = new Dictionary();
-  await dict.init();
-  await sprint.init();
-  sprint.addDictionary(dict);
-  dict.addSprint(sprint);
-  dict.preSelectLevelAndPage(
-    +localStorage.getItem(LocalStorage.dictionaryDifficultyLevel),
-    +localStorage.getItem(LocalStorage.dictionaryPageNumber),
-  );
-  const body = document.querySelector('body');
-  const modal = new Modal(body);
-
   const viewManager = new ViewManager();
+  await viewManager.init();
   viewManager.renderFooter();
   window.addEventListener('go-to-login-screen', ()=> {
     viewManager.renderHome();
-  })
-  window.addEventListener('show-error', (event: CustomEvent)=> {
-    modal.setText(event.detail.error);
-    modal.show();
   })
 }
 
--- src/scripts/manager/ViewManager.ts
+++ src/scripts/manager/ViewManager.ts
@@ -5,15 +5,20 @@ import { HeaderView } from '../header/HeaderView';
 import { FooterView } from '../footer/FooterView';
 import Dictionary from '../dictionary/Dictionary';
 import { HomeNavigation } from '../home/navigation/Navigation';
+import Sprint from "../sprint/Sprint";
+import {LocalStorage} from "../state/StorageSettings";
+import Modal from "../api/Modal";
 
 export class ViewManager {
   burger: BurgerView;
   main: MainView;
   header: HeaderView;
   footer: FooterView;
-  dictionary: Dictionary;
+  private readonly dictionary: Dictionary;
   homeNavigation: HomeNavigation;
   home: HomeView;
+  private readonly sprint: Sprint;
+  private readonly modal: Modal;
 
   constructor() {
     this.burger = new BurgerView();
@@ -23,12 +28,30 @@ export class ViewManager {
     this.header = new HeaderView();
     this.footer = new FooterView();
     this.dictionary = new Dictionary();
+    this.sprint = new Sprint();
+    this.modal = new Modal(document.querySelector('body'));
 
     this.main.render();
+    // @ts-ignore
     this.header.render(this);
     this.burger.render(this.main.mainBox, this.dictionary);
     this.home.render(this.main.mainBox);
     this.homeNavigation.render(this.main.main, this.main.mainBox);
+  }
+
+  async init() {
+    await this.dictionary.init();
+    await this.sprint.init();
+    this.sprint.addDictionary(this.dictionary);
+    this.dictionary.addSprint(this.sprint);
+    this.dictionary.preSelectLevelAndPage(
+      +localStorage.getItem(LocalStorage.dictionaryDifficultyLevel),
+      +localStorage.getItem(LocalStorage.dictionaryPageNumber),
+    );
+    window.addEventListener('show-error', (event: CustomEvent)=> {
+      this.modal.setText(event.detail.error);
+      this.modal.show();
+    })
   }
 
   renderFooter() {
