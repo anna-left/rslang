648dd4e5e064463fb8226418c4e7b969d77cb625
Valentine <valentine.nazarov@gmail.com>
2022-02-19 16:35:43 +0300

    feat: export api and dict instances for audiocall

--- src/scripts/manager/ViewManager.ts
+++ src/scripts/manager/ViewManager.ts
@@ -104,4 +104,12 @@ export class ViewManager {
       this.main.main.removeChild(this.homeNavigation.nav);
     }
   }
+
+  getDict() {
+    return this.dictionary;
+  }
+
+  getAPI() {
+    return this.api;
+  }
 }
